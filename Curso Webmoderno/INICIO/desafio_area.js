afbb10f10edc4bc0a585b1a9cbcfd0b0341c66b8
Paulo Henrique Andre <paulo.andre@effectiveerp.net>
2023-01-12 09:59:41 -0300

    variaveis

---
+++ Curso Webmoderno/INICIO/desafio_area.js
@@ -0,0 +1,9 @@
+
+const pi = 3.141592
+let raio = 10;
+let areaCirculo = pi * raio * raio;
+
+console.log(areaCirculo);
+console.log(pi);            // minha variavel
+console.log(Math.PI);       // função PI
+
